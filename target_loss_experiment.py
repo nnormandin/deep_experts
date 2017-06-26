dc5cc63d1d071bf54abdaee1479cd185560d2993
nnormandin <nicholas.e.normandin@gmail.com>
2017-06-25 21:09:44 -0400

    switch to GRU architecture for next run

--- target_loss_experiment.py
+++ target_loss_experiment.py
@@ -2,7 +2,7 @@ import utils.data_utils as util
 import keras.backend as K
 from keras.models import Sequential
 from keras.layers import Dense #, BatchNormalization #, Dropout
-from keras.layers import LSTM
+from keras.layers import GRU
 from sklearn.metrics import mean_squared_error
 import pickle
 import math
@@ -29,9 +29,9 @@ for i in [1, 5, 10, 20, 30, 40, 50, 75, 100]:
 	
 	# define graph
 	mod = Sequential()
-	mod.add(LSTM(128, return_sequences = True, input_shape = X.shape[1:]))
+	mod.add(GRU(128, return_sequences = True, input_shape = X.shape[1:]))
 	#mod.add(LSTM(128, return_sequences = True))
-	mod.add(LSTM(128))
+	mod.add(GRU(128))
 	mod.add(Dense(1))
 	
 	# compile
