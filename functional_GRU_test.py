1159961414e537af131ffd4e39c9c139c0d2c556
nnormandin <nicholas.e.normandin@gmail.com>
2017-06-26 17:05:38 -0400

    update test

--- functional_GRU_test.py
+++ functional_GRU_test.py
@@ -18,19 +18,21 @@ X1, X2, y = util.make_data_aux(instances, tplus = 20, min_len=100, max_len=100)
 # time series data goes straight into GRU
 main_input = Input(shape=X1.shape[1:], name = 'main_in')
 
-# GRU output
-gru_out = GRU(128)(main_input)
+# stacked 128-neuron GRU output
+gru_out = GRU(64, return_sequences = True)(main_input)
+gru_out = GRU(64, return_sequences = True)(gru_out)
+gru_out = GRU(64)(gru_out)
 
 # GRU output for loss calculation #1
 gru_pred = Dense(1, name = 'GRU_out')(gru_out)
 
 # merge other data with GRU output
-aux_input = Input(shape = (2,), name = 'aux_in')
+aux_input = Input(shape = X2.shape[1:], name = 'aux_in')
 x = concatenate([gru_out, aux_input])
 
 # fully connected FFN
 x = Dense(64, activation = 'elu')(x)
-x = Dense(32, activation = 'elu')(x)
+x = Dense(64, activation = 'elu')(x)
 
 # main output for loss calculation #2
 main_pred = Dense(1, name = 'main_out')(x)
@@ -40,9 +42,9 @@ model = Model(inputs=[main_input, aux_input], outputs=[main_pred, gru_pred])
 
 # weight losses and compile model
 model.compile(optimizer='adam', loss='mean_squared_error',
-              loss_weights=[.8, .2])
+              loss_weights=[.9, .1])
 
-model.fit([X1, X2], [y, y], epochs=5, batch_size=32)
+model.fit([X1, X2], [y, y], epochs=15, batch_size=32)
 
 # clear graph
 K.clear_session()
